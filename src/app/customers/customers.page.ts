fba624f3126ca300c4360f9cbef0e17d86e34eb0
rctigre <rica.castillo@duocuc.cl>
2023-10-09 19:39:46 -0300

    avance1

---
+++ src/app/customers/customers.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.page.html',
+  styleUrls: ['./customers.page.scss'],
+})
+export class CustomersPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  goToHome(){
+    this.router.navigate(['/home'])
+  }
+
+}
